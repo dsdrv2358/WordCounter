dd67541100ee453fbd0b7f3206e578ad5f32f3be
Monty <yankeesfan4life18@gmail.com>
2023-12-06 10:32:48 -0500

    ui int

--- src/main/java/PopulateDatabase.java
+++ src/main/java/PopulateDatabase.java
@@ -8,12 +8,16 @@ import bst.BSTLyricDatabase;
 
 public class PopulateDatabase {
 
+    static IntChainedHashSet rockSet = makeTheSet("lyrics/rock_lyrics.txt");
+    static IntChainedHashSet rapSet = makeTheSet("lyrics/rap_lyrics.txt");
+    static IntChainedHashSet countrySet = makeTheSet("lyrics/country_lyrics.txt");
+
     public static void main(String[] args) {
         String inputFilePath = "lyrics/rock_lyrics.txt";
         String outputFilePath = "lyrics/test.txt";
 
         IntChainedHashSet<String> mySet = new IntChainedHashSet<String>(10000);
-        // BSTLyricDatabase<String> mySet = new BSTLyricDatabase<String>();
+// BSTLyricDatabase<String> mySet = new BSTLyricDatabase<String>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
              BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
@@ -35,6 +39,33 @@ public class PopulateDatabase {
         }
         System.out.println(mySet.getNumItems());
         System.out.println(mySet.getCount("hat"));
+    }
+
+
+    public static IntChainedHashSet<String> makeTheSet(String inputFilePath){
+        String outputFilePath = "lyrics/test.txt";
+
+        IntChainedHashSet<String> mySet = new IntChainedHashSet<String>(10000);
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
+
+            String line;
+            int count = 0;
+            // Read each line from the file
+            while ((line = reader.readLine()) != null && count < 150000) {
+                // Split the line into words using whitespace
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    count++;
+                    mySet.add(word);
+                }
+            }
+
+
+        } catch (IOException e) {
+        }
+        return mySet;
     }
 
 }
